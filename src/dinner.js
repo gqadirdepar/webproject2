83111c774efa2f1f7176ed3cef85499176d03d10
gqadirdepar <38527806+gqadirdepar@users.noreply.github.com>
2020-12-16 22:02:00 +0500

    div added to overcome issue/error
    error occured because I returned two <h1> but it can return only one html element so I added one Div and included two h1 inside it.so error removed

--- src/dinner.js
+++ src/dinner.js
@@ -2,10 +2,10 @@ import React from "react";
 
 function Dinner(props){
     return (
-        
+        <div>
     <h1> Today I am serving {props.dish} </h1> ;
     <h1> Today I am serving {props.sweet} </h1>;
-
+        </div>
     );
 
 }
