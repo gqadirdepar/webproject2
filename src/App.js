6f2b31496bb62c3f9f34a68ec3186b3ca867f29d
gqadirdepar <38527806+gqadirdepar@users.noreply.github.com>
2020-12-16 22:28:58 +0500

    multiple calls of child components in
    mutiple childs added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import React from "react";
 import Dinner from "./dinner"
 function App() {
   return (
     <div className="App">
       <Dinner dish="biryani" sweet="kheer" />
+      <Dinner dish="chawal" sweet="fish ka halwa" />
+      <Dinner dish="pali" sweet="kajoor ka juice" />
+      <Dinner dish="saag" sweet="ice cream" />
     </div>
   );
 }
